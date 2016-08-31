c1a72398c170433938e25ac343823edbaa83dba0
Carlos <Carlos@cfigueroa>
2016-08-31 19:38:39 -0400

    Moved the Jframe to center of screen, as well as resiezed it.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -1,11 +1,22 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+public class Lab3Exercises 
+{
+	public static void main(String[] args) 
+	{
+		JFrame myFrame = new JFrame("Carlos Jose Figueroa Vazquez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)screen.getWidth();
+		int height = (int)screen.getHeight();
+		
+		myFrame.setLocation((width/2-100), (height/2-100));
+		
+	
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
