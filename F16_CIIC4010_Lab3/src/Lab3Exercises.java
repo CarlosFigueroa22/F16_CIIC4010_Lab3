362a107a1d3c8deb1883cdd62d07ef60c97d38b8
Carlos <Carlos@cfigueroa>
2016-08-31 20:04:59 -0400

    Added background color and some borders

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -18,5 +18,8 @@ public class Lab3Exercises
 	
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
+        MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 	}
 }
